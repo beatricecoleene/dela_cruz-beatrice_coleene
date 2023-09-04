dfbd9b2d8f867685b8c778760e5a5538e85d769e
beatricecoleene <143236199+beatricecoleene@users.noreply.github.com>
2023-09-04 23:48:14 +0800

    Update store.py

--- store.py
+++ store.py
@@ -1,22 +1,29 @@
-class Person:
-    def __init__(self,name):
-        self.name=name
-class Wallet:
-    def cash(self, cash):
-        self.cash=cash
+class Convinience_store:
+	store=" "
+	def enter_store(self, store):
+		print(f"Customer entered to {store}")
+		
 
 class Basket:
-    def __str__(self,size):
-        size=" "
-        print(f"The basket you picked is {size}")
-    
+	
+	def __init__(self):
+		print("Customer picked up a basket")
+		
+class Product:
+		def __init__(self, kind_of_product, brand, flavor, how_many, price):
+			self.kind_of_product=kind_of_product
+			self.brand=brand
+			self.flavor=flavor
+			self.how_many=how_many
+			self.price=price
+		def product(self):
+			print(f"Customer choose {self.brand} {self.kind_of_product} {self.flavor}")
+			
 
-class Shelf:
-    def products(self,product):
-        self.products=products
-
-
-
-
-if __name__=='__main__':
-    basket=Basket("mediuim")
+		
+bs= Basket		
+cs= Convinience_store()
+product= Product("Slurpee", "7/11", "Strawberry ", 1, 35)
+cs.enter_store("Seven-Eleven")
+bs()
+product.product()
